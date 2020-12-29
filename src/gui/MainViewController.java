ea110750327d3024a95bf191999fd81bffdda157
Everton Vaszelewski <everton.vaszelewski@gmail.com>
2020-12-29 12:35:45 -0300

    Adding database access

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -64,6 +64,9 @@ public class MainViewController implements Initializable {
 			mainVBox.getChildren().add(mainMenu);
 			mainVBox.getChildren().addAll(newVBox.getChildren());
 			
+			T controller = loader.getController();
+			initializingAction.accept(controller);
+			
 		} catch (IOException e) {
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
